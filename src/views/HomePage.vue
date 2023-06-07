8d5a954538006ab300fda19d680a6d138f106bef
Deyjaun Lawrence <Dlawrence@brooklynsteamcenter.org>
2023-06-07 14:53:31 -0400

    added carousel

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,3 +1,21 @@
 <template>
 <h1>Welcome To Anime Scaling</h1>
+
+<v-carousel>
+  <v-carousel-item
+    src='https://drive.google.com/file/d/18N1Ec7aJjmhRpLz4ja-eHdka8BVlsxcM/view?usp=sharing'
+    cover
+  ></v-carousel-item>
+
+  <v-carousel-item
+    src="debate.jpeg"
+    cover
+  ></v-carousel-item>
+
+  <v-carousel-item
+    src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.tiktok.com%2F%40ily.zets%2Fvideo%2F7169608743666191659&psig=AOvVaw1C6yeniNgarfqeuvZHIb6D&ust=1686244546767000&source=images&cd=vfe&ved=0CA0QjRxqFwoTCLj1wNfUsf8CFQAAAAAdAAAAABAT"
+    cover
+  ></v-carousel-item>
+</v-carousel>
+
 </template>
